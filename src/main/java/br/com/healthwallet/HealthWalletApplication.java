cce1b7b3a4754a9cea1adcc23b6116ab6daed5fb
Gabriela <gabiviana.cunha@gmail.com>
2026-04-20 14:20:13 -0300

    feat: initial project setup

---
+++ src/main/java/br/com/healthwallet/HealthWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.healthwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthWalletApplication.class, args);
+	}
+
+}
